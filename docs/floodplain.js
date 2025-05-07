594e5e767d7daf02ef3f400ca5c057d66be13061
Paul Carlson <pdcarlson06@gmail.com>
2025-05-07 15:29:43 -0400

    add floodplain labels for testing

--- docs/floodplain.js
+++ docs/floodplain.js
@@ -82,6 +82,55 @@ map.on('load', function () {
 			.addTo(map);
 	});
 
+	// floodplain labels
+	map.addLayer({
+		'id': 'floodplain-labels',
+		'type': 'symbol',
+		'source': 'floodplain',
+		'source-layer': '25001c-2014-c2ck89',
+		'layout': {
+			'text-field': [
+				'case',
+				['==', ['get', 'FLD_ZONE'], 'AE'],
+				['concat', 'AE ', ['get', 'STATIC_BFE']],
+				['==', ['get', 'FLD_ZONE'], 'VE'],
+				['concat', 'VE ', ['get', 'STATIC_BFE']],
+				['==', ['get', 'FLD_ZONE'], 'X'],
+				'X',
+				['==', ['get', 'FLD_ZONE'], 'A'],
+				'A',
+				'' // Default to no label if no match
+			],
+			'visibility': 'none',
+			'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'], // Bold font for better visibility
+			'text-size': [
+				'interpolate', 
+				['linear'], 
+				['zoom'],
+				10, 12, 
+				16, 16
+			], // Slightly larger text at higher zooms
+			'symbol-placement': 'point', // Treat each symbol as a point
+			'symbol-spacing': 80, // Closer labels for more density
+			'text-rotation-alignment': 'map', // Rotate with the map
+		},
+		'paint': {
+			'text-color': [
+				'match',
+				['get', 'FLD_ZONE'],
+				'AE', '#202020',
+				'VE', '#202020',
+				'X', '#202020',
+				'A', '#202020',
+				/* other */ '#aa0000'
+			],
+			'text-opacity': 0.6, // Make labels more visible but not overwhelming
+			'text-halo-color': '#ffffff', // Add a subtle halo
+			'text-halo-width': 1, // Increase halo width for better visibility
+			'text-halo-blur': 0.4 // Slightly larger blur for smoother edges
+		}
+	});
+
 	map.on('mouseenter', 'floodplain', function () {
 		map.getCanvas().style.cursor = 'pointer';
 	});
