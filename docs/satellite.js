50bbda5393891f5eb63dc912f3f7974bbb10c55c
Paul Carlson <pdcarlson06@gmail.com>
2025-05-14 13:09:59 -0400

    add code to create geocoder. put this code and the map init in their own file called map-init.js

--- docs/satellite.js
+++ docs/satellite.js
@@ -1,14 +1,4 @@
 // JS file for satellite layer
-
-// create a mapbox map
-mapboxgl.accessToken = 'pk.eyJ1IjoiZXNlLXRvaCIsImEiOiJja2Vhb24xNTEwMDgxMzFrYjVlaTVjOXkxIn0.IsPo5lOndNUc3lDLuBa1ZA';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/ese-toh/ckh2ss32s06i119paer9mt67h',
-    center: [-70.36, 41.660],
-    zoom: 12
-});
-
 map.on('load', function() {
     // add source and layer for raster
     map.addSource('satellite', {
