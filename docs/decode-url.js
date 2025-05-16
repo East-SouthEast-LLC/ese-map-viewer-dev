c3de70c61bb92131068f865040c2d3d98bb6aa9e
Paul Carlson <pdcarlson06@gmail.com>
2025-05-16 09:25:30 -0400

    add handling for labels with multiple map layers

--- docs/decode-url.js
+++ docs/decode-url.js
@@ -1,8 +1,5 @@
 function applyUrlParams(map) {
-    console.log("applyUrlParams function called.");
-    console.log("current URL:", window.location.search);
     const urlParams = new URLSearchParams(window.location.search);
-
 
     // Get and set zoom level
     const zoom = parseFloat(urlParams.get('zoom'));
@@ -19,28 +16,56 @@ function applyUrlParams(map) {
     }
 
     // Ensure the layers are loaded and then simulate the button click for each layer
-    
-    // console log to display the link
-    console.log("[URL] Current URL:", urlParams.toString());
-
     let layers = urlParams.get('layers')?.split(',') || [];
-    console.log("[URL] Parsed layers from URL:", layers);
-
     map.on('styledata', function () {
-        console.log("[URL] styledata event triggered. Map layers at this point:", map.getStyle().layers.map(l => l.id));
-
         layers.forEach(layerId => {
-            console.log(`[URL] Processing layer from URL: "${layerId}"`);
-
             // Check if the layer exists in the map style
             if (map.getLayer(layerId)) {
-                console.log(`[URL] Found layer: ${layerId}. Setting visibility to visible.`);
 
                 // Toggle layer visibility by changing the layout object's visibility property
                 let visibility = map.getLayoutProperty(layerId, 'visibility');
                 if (visibility === 'none') {
                     map.setLayoutProperty(layerId, 'visibility', 'visible');
                 }
+
+                // ---------------------------------------------------------------
+                // handle floodplain layers -----------------------------------
+                if (layerId === 'floodplain') {
+                    // toggle the limwa visibility to the same as the floodplain visibility
+                    map.setLayoutProperty('LiMWA', 'visibility', visibility);
+                    map.setLayoutProperty('floodplain-line', 'visibility', visibility);
+                    map.setLayoutProperty('floodplain-labels', 'visibility', visibility);
+                }
+
+                // handle dep wetland layers -----------------------------------
+                if (layerId === 'DEP wetland') {
+                    // toggle the dep line and labels visibility to the same as the dep wetland visibility
+                    map.setLayoutProperty('dep-wetland-line', 'visibility', visibility);
+                    map.setLayoutProperty('dep-wetland-labels', 'visibility', visibility);
+                }
+
+                // handle soils layers ----------------------------------------
+                if (layerId === 'soils') {
+                    // toggle the soils line and labels visibility to the same as the soils visibility
+                    map.setLayoutProperty('soils-labels', 'visibility', visibility);
+                    map.setLayoutProperty('soils-outline', 'visibility', visibility);
+                }
+
+                // handle zone II layers ----------------------------------------
+                if (layerId === 'zone II') {
+                    // toggle the zone II line and labels visibility to the same as the zone II visibility
+                    map.setLayoutProperty('zone-ii-outline', 'visibility', visibility);
+                    map.setLayoutProperty('zone-ii-labels', 'visibility', visibility);
+                }
+
+                // handle endangered species layers -----------------------
+                if (clickedLayer === 'endangered species') {
+                    // toggle the endangered species labels visibility to the same as the endangered species visibility
+                    map.setLayoutProperty('endangered-species-labels', 'visibility', visibility);
+                    map.setLayoutProperty('vernal-pools', 'visibility', visibility);
+                    map.setLayoutProperty('vernal-pools-labels', 'visibility', visibility);
+                }
+                // ------------------------------------------------------------
 
                 // Simulate button click for toggling button colors
                 let buttonList = document.querySelectorAll('a'); // Get all buttons
@@ -50,8 +75,7 @@ function applyUrlParams(map) {
                         button.classList.add('active');
                     }
                 });
-            }
-            else {
+            } else {
                 console.warn(`[URL] Layer "${layerId}" not found in the map style.`);
             }
         });
