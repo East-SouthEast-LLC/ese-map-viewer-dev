889290b885a82ce828524072cd915ae9126f9a40
Paul Carlson <pdcarlson06@gmail.com>
2025-07-01 11:55:22 -0400

    fix(usgs-quad): correct typo in palette variable and improve handling of paletted images

--- docs/layers/usgs-quad.js
+++ docs/layers/usgs-quad.js
@@ -3,7 +3,6 @@ async function addUsgsQuadLayer() {
     const geoTiffUrl = 'https://east-southeast-llc.github.io/ese-map-viewer/data/USGS-test2.tif'; 
 
     try {
-        console.log(`[${layerId}] Starting to fetch GeoTIFF...`);
         const response = await fetch(geoTiffUrl);
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
         
@@ -24,25 +23,22 @@ async function addUsgsQuadLayer() {
         const rasters = await image.readRasters();
         const photometricInterpretation = image.fileDirectory.PhotometricInterpretation;
 
-        console.log(`[${layerId}] Photometric Interpretation: ${photometricInterpretation}`);
-
-        // Handle Paletted (indexed-color) images
-        if (photometricInterpretation === 3) {
+        if (photometricInterpretation === 3) { // 3 means Paletted
             console.log(`[${layerId}] Detected Paletted Image. Using Color Map.`);
             const colorMap = image.fileDirectory.ColorMap;
-            // This line was corrected from 'const-palette' to 'const palette'
-            const palette = rasters[0]; 
+            // --- CORRECTED LOGIC ---
+            const paletteData = rasters[0];
             let j = 0;
-            for (let i = 0; i < palette.length; i++) {
-                const index = palette[i];
-                imageData.data[j++] = (colorMap[index][0] / 65535) * 255;
-                imageData.data[j++] = (colorMap[index][1] / 65535) * 255;
-                imageData.data[j++] = (colorMap[index][2] / 65535) * 255;
-                imageData.data[j++] = 255;
+            for (let i = 0; i < paletteData.length; i++) {
+                const index = paletteData[i];
+                // Scale color values from 0-65535 range to 0-255 range
+                imageData.data[j++] = (colorMap[index][0] / 65535) * 255; // Red
+                imageData.data[j++] = (colorMap[index][1] / 65535) * 255; // Green
+                imageData.data[j++] = (colorMap[index][2] / 65535) * 255; // Blue
+                imageData.data[j++] = 255; // Alpha (fully opaque)
             }
         } 
-        // Handle standard RGB images
-        else if (photometricInterpretation === 2) {
+        else if (photometricInterpretation === 2) { // 2 means RGB
             console.log(`[${layerId}] Detected RGB Image.`);
             const [R, G, B] = rasters;
             let j = 0;
