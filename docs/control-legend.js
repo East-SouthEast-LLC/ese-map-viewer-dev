7aed1adedc38955c63754a954972239b36ee615d
Paul Carlson <pdcarlson06@gmail.com>
2025-08-12 17:02:01 -0400

    replace dev filepaths

--- docs/control-legend.js
+++ docs/control-legend.js
@@ -212,7 +212,7 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
     }
 
-    fetch('https://east-southeast-llc.github.io/ese-map-viewer-dev/docs/legend-data.json')
+    fetch('https://east-southeast-llc.github.io/ese-map-viewer/docs/legend-data.json')
         .then(response => response.json())
         .then(data => {
             legendData = data;
