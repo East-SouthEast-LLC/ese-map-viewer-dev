f6ed4d5902bc2a5838c753076d7b931feaa258fb
Paul Carlson <pdcarlson06@gmail.com>
2025-08-13 16:57:35 -0400

    refine geocoder spacing

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ function adjustLayout() {
 
   const headerHeight = header.offsetHeight;
   const buffer = 70; // a 70px buffer to make the map shorter
-  const topOffset = headerHeight + 45; // a 45px buffer below the header
+  const topOffset = headerHeight + 40; // a 40px buffer below the header
 
   // calculate the available height for the map
   const availableHeight = window.innerHeight - headerHeight - buffer;
